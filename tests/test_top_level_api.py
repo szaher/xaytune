bb5096653d6fae4cb32aa55b5f09ca41b1febdf7
Saad Zaher <szaher@redhat.com>
2026-05-22 20:29:08 +0100

    feat: complete top-level API with evaluate, export, and all recipes

--- tests/test_top_level_api.py
+++ tests/test_top_level_api.py
@@ -1,4 +1,5 @@
 import trainlib
+from trainlib import export
 
 
 class TestTopLevelAPI:
@@ -11,6 +12,17 @@ class TestTopLevelAPI:
     def test_pretrain_importable(self):
         assert callable(trainlib.pretrain)
 
+    def test_align_importable(self):
+        assert callable(trainlib.align)
+
+    def test_evaluate_importable(self):
+        assert callable(trainlib.evaluate)
+
+    def test_export_module(self):
+        assert callable(export.merge)
+        assert callable(export.save)
+        assert callable(export.push_to_hub)
+
     def test_finetune_is_recipe(self):
         from trainlib.recipes.finetune import finetune
         assert trainlib.finetune is finetune
@@ -18,3 +30,11 @@ class TestTopLevelAPI:
     def test_pretrain_is_recipe(self):
         from trainlib.recipes.pretrain import pretrain
         assert trainlib.pretrain is pretrain
+
+    def test_align_is_recipe(self):
+        from trainlib.recipes.align.align import align
+        assert trainlib.align is align
+
+    def test_evaluate_is_eval(self):
+        from trainlib.eval.evaluate import evaluate
+        assert trainlib.evaluate is evaluate
--- trainlib/__init__.py
+++ trainlib/__init__.py
@@ -2,8 +2,9 @@
 
 __version__ = "0.1.0"
 
+from trainlib.eval import evaluate
 from trainlib.recipes.align import align
 from trainlib.recipes.finetune import finetune
 from trainlib.recipes.pretrain import pretrain
 
-__all__ = ["__version__", "align", "finetune", "pretrain"]
+__all__ = ["__version__", "align", "evaluate", "finetune", "pretrain"]
